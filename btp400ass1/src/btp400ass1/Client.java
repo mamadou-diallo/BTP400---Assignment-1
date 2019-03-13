4cdb9ef80245cb4b4a6cde1e5c1b842a9c00008d
nashraf5 <48459214+nashraf5@users.noreply.github.com>
2019-03-12 20:39:14 -0400

    Create Client.java

---
+++ btp400ass1/src/btp400ass1/Client.java
@@ -0,0 +1,43 @@
+package btp400ass1;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        
+    	//get the localhost IP address, if server is running on some other IP, you need to use that
+        InetAddress host = InetAddress.getLocalHost();
+        Socket socket = null;
+        ObjectOutputStream outputStream = null;
+        ObjectInputStream inputStream = null;
+        
+        for (int i = 0; i < 5; i++) {
+            //establish socket connection to server
+            socket = new Socket(host.getHostName(), port);
+            
+            //write to socket using ObjectOutputStream
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            System.out.println("Sending request to Socket Server");
+            if (i == 4)
+            	outputStream.writeObject("exit");
+            else 
+            	outputStream.writeObject("" + i);
+            
+            //read the server response message
+            inputStream = new ObjectInputStream(socket.getInputStream());
+            String message = (String) inputStream.readObject();
+            System.out.println("Message: " + message);
+            
+            //close resources
+            inputStream.close();
+            outputStream.close();
+            Thread.sleep(100);
+        }
+    }
+}
