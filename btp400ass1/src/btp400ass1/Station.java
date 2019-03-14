3317004d74e4f98d8485641c576eae7820afa9f3
mamadou.diallo <mamadou.diallo@hotmail.com>
2019-03-13 21:20:36 -0400

    .

--- btp400ass1/src/btp400ass1/Station.java
+++ btp400ass1/src/btp400ass1/Station.java
@@ -1,18 +1,23 @@
 package btp400ass1;
 
+import java.io.IOException;
+
 public class Station {
 	
 	public static int sId = 0;
 	public String stationName;
 	public int stationId;
 	public boolean status;
+	public Client client;
 	
-	public Station()
+	public Station() throws ClassNotFoundException, IOException
 	{
 		sId++;
 		stationId = sId;
 		stationName = "";
 		status = true;
+		client = new Client();
+		
 	}
 	public String getStationName() {
 		return stationName;
